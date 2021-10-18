74ddffa4cdacb402c5aa3835e98dc4a282587f7d
Rawan <Rawan.Abdullah.ruh.k@tuwaiq.edu.sa>
2021-10-18 07:33:02 +0300

    anonymous function

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+import kotlin.random.Random
+import kotlin.random.nextInt
+
+fun main(args: Array<String>) {
+ //ananymous function
+val numLetters = "mmmmmgggdd".count({letter->letter=='s'})
+ println(numLetters)
+ 
+}
+
+
+
